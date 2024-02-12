584f545a5d680692357673ab9d65523ed9eec4eb
David A <avila_david_2@yahoo.com>
2024-02-11 20:14:32 -0600

    Update app.js
    Updated code in app.js

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 //Object with properties
-var person = {
-    firstName: "David",
-    lastName: "A",
-    age: 25
-};
+const age = 25;
 
-//Functions
-function birthdayParty(age){
-    console.log("It's Party Time!");
-    return age+1;
+const name = "Davidos";
+
+console.log("Hello my name is "+name+" and my age is "+age);
+
+//If Else
+if(age > 18){
+    console.log("Your age is "+age+". You are older than 18.");
+}else if(age < 15){
+    console.log("Your age is "+age+". You are really young.");
+}else {
+    console.log("Your age is "+age+". You are not older than 18");
 }
 
-function shout(greeting){
-    //Method
-    return greeting.toUpperCase();
+//Switch Case and String Method
+switch(name.length){
+    case 1:
+    case 2:
+        console.log("Your name is really short");
+        break;
+    case 3:
+    case 4:
+        console.log("Your name is short");
+        break;
+    case 5:
+    case 6:
+        console.log("You have a normal length name");
+        break;
+    default:
+        //Number Method    
+        console.log("Your name is long. It is "+name.length.toString()+" characters.");
+        break;
 }
-
-//Event
-person["age"] = birthdayParty(person["age"]);
-//Error Checking & Printing
-console.log(person["age"]);
-console.log(shout("hey"));
