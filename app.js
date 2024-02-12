5371caa6bae25b2cc977cfe41b2bd1ba938fb664
David A <avila_david_2@yahoo.com>
2024-02-12 14:04:32 -0600

    Update app.js
    Updated the app.js file to include previous assignment code that was replaced when new assignment code was pushed. Pulled old code from repository history.

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+/********************/
+/*** Assignment 3 ***/
+/********************/
+//Object with properties
+var person = {
+    firstName: "David",
+    lastName: "A",
+    age: 25
+};
+
+//Functions
+function birthdayParty(age){
+    console.log("It's Party Time!");
+    return age+1;
+}
+
+function shout(greeting){
+    //Method
+    return greeting.toUpperCase();
+}
+
+//Event
+person["age"] = birthdayParty(person["age"]);
+//Error Checking & Printing
+console.log(person["age"]);
+console.log(shout("hey"));
+
+/********************/
+/*** Assignment 4 ***/
+/********************/
+
 //Object with properties
 const age = 25;
 
